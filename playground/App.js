cb81ec767bd88dd6ad63a6b5b047641209606b06
Nicolas Couvrat <nicolas.couvrat@mines-paristech.fr>
2017-12-14 13:24:28 +0900

    clean console.logs on master and added onJoystickMove callback

--- playground/App.js
+++ playground/App.js
@@ -5,9 +5,16 @@ import { JoystickDemuxed, TouchEventDemuxer, DummyElement } from 'component-lib'
 
 const Demuxer = TouchEventDemuxer([JoystickDemuxed, JoystickDemuxed]);
 
+const firstHandler = (xProp, yProp) => {
+  console.log(`first joystick: ${xProp}, ${yProp}`);
+}
+
+const secondHandler = (xProp, yProp) => {
+  console.log(`second joystick: ${xProp}, ${yProp}`);
+}
+
 export default class App extends React.Component {
   render() {
-    console.log(JoystickDemuxed.prototype)
     return (
       <Demuxer
         childrenProps={[
@@ -17,12 +24,14 @@ export default class App extends React.Component {
             length: 75,
             shape: 'circular',
             isSticky: true,
+            onJoystickMove: firstHandler,
           },
           {
             neutralPointX: 200,
             neutralPointY: 300,
             length: 50,
-            shape: 'vertical'
+            shape: 'vertical',
+            onJoystickMove: secondHandler,
           },
         ]}
       />
